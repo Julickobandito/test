3a58d989d9d8e2d297be87bf46a6f5c703d0d171
Julia <example@gmail.com>
2022-07-01 13:24:31 +0300

    index.html edited again

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let question = prompt('What is your name?');
-console.log(`Name inputted ${question}`);
+const numberOfFilms = +prompt ("How many films have you already watched?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('What was one of the last watched films?', ''),
+      b = prompt('How would you rate it?', ''),
+      e = prompt('What film?', ''),
+      f = prompt ('What actor did you like the most?', ''),
+      g = prompt ('what your favorite genre?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.actors[e] = f;
+personalMovieDB.genres[0] = g;
+console.log(personalMovieDB);
